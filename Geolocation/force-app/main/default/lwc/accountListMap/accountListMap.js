0f9aeb8d769008decad84b6a146286e2e98a135f
SamuelPique <58191096+SamuelPique@users.noreply.github.com>
2019-11-29 09:25:09 +0100

    Add files via upload
    Vendredi 29 Octobre 2019.
    Mise à jour du code avec les composants AccountMap et AccountListMap.

--- Geolocation/force-app/main/default/lwc/accountListMap/accountListMap.js
+++ Geolocation/force-app/main/default/lwc/accountListMap/accountListMap.js
@@ -1,7 +1,27 @@
 import { LightningElement,wire,track } from 'lwc';
 import getAllParisCustomer from '@salesforce/apex/AccountData.getAllParisCustomer';
 
+
 export default class AccountListMap extends LightningElement {
-    @track accounts;
-    @wire(getAllParisCustomer)accounts;
+    @track columnList = [
+        {label: 'Id', fieldName: 'Id'},
+        {label: 'Name', fieldName: 'Name'},
+        {label: 'BillingCity', fieldName: 'BillingCity'},
+        {label: 'BillingCountry', fieldName: 'BillingCountry'},
+        {label: 'BillingPostalCode', fieldName: 'BillingPostalCode'},
+        {label: 'BillingState', fieldName: 'BillingState'},
+        {label: 'BillingStreet', fieldName: 'BillingStreet'}
+    ];
+    @track error;
+    @track accountList;
+    @wire(getAllParisCustomer)
+    wiredAccounts({error, data}){
+        if (data){
+            this.accountList = data;
+            this.error = undefined;          
+        }else if(error){           
+            this.accountList = undefined;
+            this.error = error;
+        }
+    }
 }
